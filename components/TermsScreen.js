eef911c2a15ddbb6df1d760fb74bfed222bfba4b
Belgin Android <belginprogrammerterno@gmail.com>
2020-11-17 18:04:40 +0530

    Update TermsScreen.js

--- components/TermsScreen.js
+++ components/TermsScreen.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React from "react"
 import {
-  View,
-  Text,
   SafeAreaView,
-  TouchableOpacity,
+  Text,
+  View,
   Dimensions,
+  StyleSheet
 } from "react-native"
 
 const Dev_Height = Dimensions.get('window').height
 const Dev_Width = Dimensions.get('window').width
 
 export default class TermsScreen extends React.Component{
-   render(){
-    return (
-      <SafeAreaView>
-       <Text> Hi </Text>
+  render(){
+    return(
+      <SafeAreaView style={styles.container}>
+        <Text> Hi I am Belgin </Text>
       </SafeAreaView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    height:Dev_Height,
+    width:Dev_Width
+  }
+})
