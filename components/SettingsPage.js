9527a6f2daa0a045e07bc8cadc200188558a88a0
Belgin Android <belginprogrammerterno@gmail.com>
2021-10-31 21:20:59 +0530

    Settings , Terms Code Formated

--- components/SettingsPage.js
+++ components/SettingsPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { 
-  Text, 
-  View, 
-  StyleSheet, 
-  SafeAreaView, 
-  Dimensions, 
-  Image ,
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Dimensions,
+  Image,
   TouchableOpacity,
   Switch,
   Linking,
@@ -22,74 +22,74 @@ import Icon from 'react-native-vector-icons/Feather';
 
 export default class SettingScreen extends React.Component {
 
-      state={
-        visible : true,
-        NotificationSwitch:false,
-      }
+  state = {
+    visible: true,
+    NotificationSwitch: false,
+  }
 
-  render(){
-    return (  
-      <View style={{height:Dev_Height,width:Dev_Width,backgroundColor:"#1A1A1E"}}>
-      <StatusBar backgroundColor="#1A1A1E" barStyle="light-content" />
+  render() {
+    return (
+      <View style={{ height: Dev_Height, width: Dev_Width, backgroundColor: "#1A1A1E" }}>
+        <StatusBar backgroundColor="#1A1A1E" barStyle="light-content" />
 
-      <View style={{...styles.settings_text_view,marginTop:"5%"}}>
+        <View style={{ ...styles.settings_text_view, marginTop: "5%" }}>
           <Text style={styles.settings_text_style}>About the App</Text>
-        </View> 
-        
-        <TouchableOpacity style={{height:"8%",width:"100%",alignItems:"center",flexDirection:"row",marginTop:10}}
-         onPress={()=>this.props.navigation.navigate("Terms")}>
-          <Icon name="info" color="#FFF" size={24} style={{marginLeft:"10%"}} />
-          <Text style={{color:"#FFF",fontSize:18,marginLeft:"8%"}} >Terms Of Use</Text>
+        </View>
+
+        <TouchableOpacity style={{ height: "8%", width: "100%", alignItems: "center", flexDirection: "row", marginTop: 10 }}
+          onPress={() => this.props.navigation.navigate("Terms")}>
+          <Icon name="info" color="#FFF" size={24} style={{ marginLeft: "10%" }} />
+          <Text style={{ color: "#FFF", fontSize: 18, marginLeft: "8%" }} >Terms Of Use</Text>
         </TouchableOpacity>
 
 
-        <TouchableOpacity style={{height:"8%",width:"100%",alignItems:"center",flexDirection:"row",marginTop:10}}
-         onPress={()=>this.props.navigation.navigate("disclaimer")}>
-          <Icon name="italic" color="#FFF" size={24} style={{marginLeft:"10%"}} />
-          <Text style={{color:"#FFF",fontSize:18,marginLeft:"8%"}} >Disclaimer</Text>
+        <TouchableOpacity style={{ height: "8%", width: "100%", alignItems: "center", flexDirection: "row", marginTop: 10 }}
+          onPress={() => this.props.navigation.navigate("disclaimer")}>
+          <Icon name="italic" color="#FFF" size={24} style={{ marginLeft: "10%" }} />
+          <Text style={{ color: "#FFF", fontSize: 18, marginLeft: "8%" }} >Disclaimer</Text>
         </TouchableOpacity>
 
-        <View style={{...styles.settings_text_view}}>
+        <View style={{ ...styles.settings_text_view }}>
           <Text style={styles.settings_text_style}>Support</Text>
-        </View> 
+        </View>
 
-        <TouchableOpacity style={{height:"8%",width:"100%",justifyContent:"center"}} 
-         onPress={()=>Linking.openURL("https://forms.gle/bU83vnQj8fBnDKPg7")}>
-          <Text style={{marginLeft:"12%",fontSize:18,color:"#FFF"}}>Report A Bug</Text>
+        <TouchableOpacity style={{ height: "8%", width: "100%", justifyContent: "center" }}
+          onPress={() => Linking.openURL("https://forms.gle/bU83vnQj8fBnDKPg7")}>
+          <Text style={{ marginLeft: "12%", fontSize: 18, color: "#FFF" }}>Report A Bug</Text>
         </TouchableOpacity>
 
-        <View style={{height:"8%",width:"100%",alignItems:"center",flexDirection:"row"}}>
-            <Text style={{marginLeft:"12%",fontSize:18,color:"#FFF"}}>App Version</Text>
-            <Text style={{marginLeft:"30%",fontSize:18,color:"gray"}}>1.0</Text>
-          </View>
+        <View style={{ height: "8%", width: "100%", alignItems: "center", flexDirection: "row" }}>
+          <Text style={{ marginLeft: "12%", fontSize: 18, color: "#FFF" }}>App Version</Text>
+          <Text style={{ marginLeft: "30%", fontSize: 18, color: "gray" }}>1.0</Text>
+        </View>
 
-    </View>
-  );
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    height:Dev_Height-"10%",
-    width:Dev_Width,
-    backgroundColor:"#FFF"
+  container: {
+    flex: 1,
+    height: Dev_Height - "10%",
+    width: Dev_Width,
+    backgroundColor: "#FFF"
   },
-  settings_text_view:{
-    height:60,
-    width:"100%",
-    justifyContent:"center",
+  settings_text_view: {
+    height: 60,
+    width: "100%",
+    justifyContent: "center",
   },
-  settings_text_style:{
-    fontSize:25,
-    fontWeight:"bold",
-    color:"#5D7FEB",
-    marginLeft:"10%"
+  settings_text_style: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#5D7FEB",
+    marginLeft: "10%"
   },
-  main_style_box:{
-    flexDirection:"row",
-    alignItems:"center",
-    height:"10%",
-    width:"100%",
+  main_style_box: {
+    flexDirection: "row",
+    alignItems: "center",
+    height: "10%",
+    width: "100%",
   }
 });
--- components/TermsScreen.js
+++ components/TermsScreen.js
@@ -16,21 +16,21 @@ const Dev_Width = Dimensions.get('window').width
 
 import Icon from "react-native-vector-icons/AntDesign"
 
-export default class TermsScreen extends React.Component{
+export default class TermsScreen extends React.Component {
 
-  OnBack=()=>{
+  OnBack = () => {
     this.props.navigation.goBack()
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <SafeAreaView style={styles.container}>
-        <StatusBar backgroundColor="#1A1A1F" barStyle="light-content"/>
+        <StatusBar backgroundColor="#1A1A1F" barStyle="light-content" />
         <View style={styles.inside_email_box_view}>
-          <Image 
-            source={{uri:"https://media-public.canva.com/adoso/MAB3gKadoso/2/tl.png"}} 
-            resizeMode="contain" 
-            style={styles.image_view} 
+          <Image
+            source={{ uri: "https://media-public.canva.com/adoso/MAB3gKadoso/2/tl.png" }}
+            resizeMode="contain"
+            style={styles.image_view}
           />
         </View>
 
@@ -38,11 +38,11 @@ export default class TermsScreen extends React.Component{
           <ScrollView>
             <Text style={styles.text_style}>{'By using our services you agree to our Privacy policy which describes how we store information. \n\n Your temporary e-mail address is completely anonymous. Your email id automatically self-destructs as time elapses.\n\nTmp Mail does not permit the users to add images in the mails due to the following reasons: \n\n 1. Online theft \n 2. Misuse of the the app for other purpose \n 3. The images may contain virus \n\n The temporary email address that you can get at Tmp Mail can serve a great number of purposes. Its main function is to protect your confidentiality when browsing the Internet and Made Only for verification purposes.'}</Text>
           </ScrollView>
-        </View>        
+        </View>
 
         <View style={styles.google_signin_view}>
           <TouchableOpacity style={styles.google_signin_buttom} onPress={this.OnBack}>
-            <Icon name="heart" size={20} color="#FFF" style={{marginLeft:"5%"}}/>
+            <Icon name="heart" size={20} color="#FFF" style={{ marginLeft: "5%" }} />
             <Text style={styles.google_signin_text}> Ok Take Me Back </Text>
           </TouchableOpacity>
         </View>
@@ -52,54 +52,54 @@ export default class TermsScreen extends React.Component{
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    height:Dev_Height,
-    width:Dev_Width,
-    backgroundColor:"#1A1A1F"
+  container: {
+    flex: 1,
+    height: Dev_Height,
+    width: Dev_Width,
+    backgroundColor: "#1A1A1F"
   },
-  image_view:{
-    height:"100%",
-    width:"100%"
+  image_view: {
+    height: "100%",
+    width: "100%"
   },
-  inside_email_box_view:{
-    height:"20%",
-    width:"100%",
-    marginTop:"10%"
+  inside_email_box_view: {
+    height: "20%",
+    width: "100%",
+    marginTop: "10%"
   },
-  text_main_view:{
-    height:"50%",
-    width:"90%",
-    marginLeft:"5%",
-    marginTop:"10%",
+  text_main_view: {
+    height: "50%",
+    width: "90%",
+    marginLeft: "5%",
+    marginTop: "10%",
   },
-  text_style:{
-    color:"#FFF",
-    fontSize:16,
+  text_style: {
+    color: "#FFF",
+    fontSize: 16,
   },
-  google_signin_view:{
-    height:"20%",
-    width:"100%",
-    justifyContent:"center",
-    alignItems:"center",
+  google_signin_view: {
+    height: "20%",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  google_signin_buttom:{
-    height:"35%",
-    width:"80%",
-    backgroundColor:"#657ee4",
-    borderRadius:10,
-    justifyContent:"center",
-    alignItems:"center",
-    flexDirection:"row",
+  google_signin_buttom: {
+    height: "35%",
+    width: "80%",
+    backgroundColor: "#657ee4",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
     shadowColor: '#1A1A1F',
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.8,
-    shadowRadius: 2,  
+    shadowRadius: 2,
     elevation: 5
   },
-  google_signin_text:{
-    fontSize:14,
-    color:"#FFF",
-    marginLeft:"5%"
+  google_signin_text: {
+    fontSize: 14,
+    color: "#FFF",
+    marginLeft: "5%"
   },
 })
