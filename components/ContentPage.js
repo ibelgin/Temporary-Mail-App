8c0378dba499c47164b7f55c6764ddae5a78fd92
Belgin Android <belginprogrammerterno@gmail.com>
2021-10-31 21:16:37 +0530

    Content Page Code Formated

--- components/ContentPage.js
+++ components/ContentPage.js
@@ -15,54 +15,54 @@ import {
 import HTML from 'react-native-render-html';
 import SnackBar from 'react-native-snackbar-component'
 
-export default class ContentPage extends React.Component{
+export default class ContentPage extends React.Component {
 
-  UNSAFE_componentWillMount(){
+  UNSAFE_componentWillMount() {
     this.SeeMessageText()
   }
 
-  SeeMessage=()=>{
+  SeeMessage = () => {
     this.props.navigation.navigate("Terms")
-    this.setState({ visiblity : false })
+    this.setState({ visiblity: false })
   }
 
-   SeeMessageText=()=>{
-    fetch("https://www.1secmail.com/api/v1/?action=readMessage&login="+this.state.email_name+"&domain="+this.state.email_domain+"&id="+this.state.id)
+  SeeMessageText = () => {
+    fetch("https://www.1secmail.com/api/v1/?action=readMessage&login=" + this.state.email_name + "&domain=" + this.state.email_domain + "&id=" + this.state.id)
       .then((response) => response.json())
       .then((json) => {
-        this.setState({ isLoading : true})
-        this.setState({ data : json["textBody"]})
-        this.setState({ visible : false})
+        this.setState({ isLoading: true })
+        this.setState({ data: json["textBody"] })
+        this.setState({ visible: false })
       })
       .catch((error) => console.error(error))
-      .finally(() => this.setState({isLoading : false}))
+      .finally(() => this.setState({ isLoading: false }))
   }
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      data:"",
-      isLoading:true,
-      email_name:this.props.route.params.email_name,
-      email_domain:this.props.route.params.email_domain,
-      id:this.props.route.params.email_id,
-      visiblity:true
+    this.state = {
+      data: "",
+      isLoading: true,
+      email_name: this.props.route.params.email_name,
+      email_domain: this.props.route.params.email_domain,
+      id: this.props.route.params.email_id,
+      visiblity: true
     }
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <>
-      <StatusBar backgroundColor="#1A1A1F" barStyle="light-content"/>
+        <StatusBar backgroundColor="#1A1A1F" barStyle="light-content" />
         <ScrollView style={styles.container} >
           {this.state.isLoading ? <ActivityIndicator style={styles.ActivityIndicator_Style} color="#FFF" /> : (
             <View style={styles.text_container}>
-              <Text  selectable={true} style={styles.text}>{this.state.data}</Text>
+              <Text selectable={true} style={styles.text}>{this.state.data}</Text>
             </View>
-          )}  
+          )}
         </ScrollView>
-        <SnackBar 
-          visible={this.state.visiblity} 
+        <SnackBar
+          visible={this.state.visiblity}
           textMessage="Only Text Will Be Shown " actionHandler={this.SeeMessage} actionText="Know More"
           backgroundColor="#121212" messageColor="#FFF" accentColor="#FFF" autoHidingTime={3000}
         />
@@ -72,23 +72,23 @@ export default class ContentPage extends React.Component{
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:"#1A1A1F",
-    height:"90%",
-    width:"100%",
+  container: {
+    flex: 1,
+    backgroundColor: "#1A1A1F",
+    height: "90%",
+    width: "100%",
   },
-  ActivityIndicator_Style:{
-    marginTop:"20%"
+  ActivityIndicator_Style: {
+    marginTop: "20%"
   },
-  text_container:{
-    height:"90%",
-    width:"90%",
-    marginLeft:"8%",
-    marginTop:"10%"
+  text_container: {
+    height: "90%",
+    width: "90%",
+    marginLeft: "8%",
+    marginTop: "10%"
   },
-  text:{
-    color:"#FFF"
+  text: {
+    color: "#FFF"
   }
 })
 
